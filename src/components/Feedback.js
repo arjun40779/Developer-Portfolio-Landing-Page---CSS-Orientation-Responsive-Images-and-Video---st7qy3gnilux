403d8049c127286fa0a4be1ef32147f57bbe2539
arjun40779 <arjun40779@gmail.com>
2023-11-13 12:28:16 +0530

    update

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,55 +1,26 @@
 export function Feedback() {
   const greet = (e) => {
-    /*  e.preventDefault(); */
     alert("Thank you for your response!");
   };
   return (
     <div>
       <form
-        className="feedback"
-        action="https://jmwf8s-3000.csb.app/contact?"
-        onSubmit={greet}
+        action="https://formspree.io/f/xwkdjpok"
+        method="POST"
+        className="form"
       >
-        <div className="name">
-          <label htmlFor="fname"> Your Name</label>
+        <label>
+          Your email:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Your message:
+          <textarea name="message"></textarea>
+        </label>
 
-          <input
-            className="fname"
-            id="fname"
-            type="text"
-            placeholder="First name"
-          />
-          <input
-            className="lname"
-            id="lname"
-            type="text"
-            placeholder="Last name"
-          />
-        </div>
-        <div className="email">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            placeholder="ex: myname@example.com"
-            id="email"
-          ></input>
-        </div>
-        <div className="phone">
-          <label htmlFor="code">Phone</label>
-          <input
-            id="code"
-            type="text"
-            defaultValue="+91"
-            className="code"
-          ></input>
-          <input
-            type="tel"
-            className="tel-phone"
-            placeholder="phone no"
-            id="phone"
-          ></input>
-        </div>
-        <input type="submit" id="submit"></input>
+        <button type="submit" id="submit">
+          Send
+        </button>
       </form>
     </div>
   );
